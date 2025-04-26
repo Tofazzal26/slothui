d3ccdb3188144103e290053ca159cf3497b7e791
Tofazzal Hossain <mdtofazzalislam6789@gmail.com>
2025-04-27 02:00:46 +0600

    banner some problem solve

--- custom.js
+++ custom.js
@@ -5,3 +5,33 @@ menuOpen.addEventListener("click", () => {
   menuList.classList.toggle("menuActive");
   menuOpen.classList.toggle("menuShowActive");
 });
+
+fetch("/review.json")
+  .then((res) => res.json())
+  .then((data) => {
+    const review = document.getElementById("review");
+    data.forEach((item) => {
+      const div = document.createElement("div");
+      div.innerHTML = `<div>
+      <div class="starParent">
+        <img src="/images/star.svg" alt="" />
+        <img src="/images/star.svg" alt="" />
+        <img src="/images/star.svg" alt="" />
+        <img src="/images/star.svg" alt="" />
+        <img src="/images/star.svg" alt="" />
+      </div>
+      <p class="peopleParagraph">
+        ${item?.paragraph}
+      </p>
+      <div class="userParent">
+        <img src="${item?.image}" alt="" />
+        <div>
+          <h2 class="userName">${item?.name}</h2>
+          <p class="userStatus">${item?.title}</p>
+        </div>
+      </div>
+    </div>`;
+      div.classList.add("card");
+      review.appendChild(div);
+    });
+  });
